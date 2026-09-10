c30bbe729f1727a0b7b6183ece16b5821ee53f35
CallMeGodsent <akpovigreat@gmail.com>
2026-09-10 17:10:33 +0100

    Add logging to config.php

--- config.php
+++ config.php
@@ -4,7 +4,10 @@
 #######################
 # 1. ESSENTIALS
 #######################
-
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+ini_set('error_log', __DIR__ . '/php_error.log');
 
 
 #######################
@@ -24,8 +27,6 @@ $host        =    "localhost";
 $dbuser      =    "root";
 $dbpass      =    "";
 $db          =    "hospa_db";
-
-
 
 
 
